dd7b26343db60632a9271bc6ee17d8a3fc1bf6ee
Fabio G Rodriguez <fabio.ger@gmail.com>
2016-11-11 09:50:48 -0300

    Premium Request Grid

--- app/app.component.ts
+++ app/app.component.ts
@@ -8,14 +8,14 @@ import { Component } from '@angular/core';
     <nav>
       <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
       <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
-      <a routerLink="/grid" routerLinkActive="active">Grid</a>
+      <a routerLink="/grid" routerLinkActive="active">Premium Request Grid</a>
     </nav>
     <router-outlet></router-outlet>
   `,
   styleUrls: ['app.component.css'],
 })
 export class AppComponent {
-  title = 'Tour of Heroes';
+  title = 'Premium Request ';
 }
 
 
--- app/services/DataSvc.js
+++ app/services/DataSvc.js
@@ -18,11 +18,11 @@ var DataSvc = (function () {
         var countries = 'US,Germany,UK,Japan,Italy,Greece'.split(','), data = new wijmo.collections.ObservableArray();
         for (var i = 0; i < count; i++) {
             data.push({
-                id: i,
-                country: countries[i % countries.length],
+                client: "String",
+                dueDate: 100,
                 date: new Date(2014, i % 12, i % 28),
                 amount: Math.random() * 10000,
-                active: i % 4 == 0
+                active: i % 4 == 0,
             });
         }
         return data;
